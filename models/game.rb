fb50d80631c68f0393c64f247bb03c93db5f5008
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-16 16:20:24 +0100

    p1 loses rock over paper

--- models/game.rb
+++ models/game.rb
@@ -2,6 +2,7 @@ class Game
 
   def self.play(input1, input2)
     return "Rock wins!" if input1 == "rock" && input2 == "scissors"
+    return "Paper wins!" if input1 == "rock" && input2 == "paper"
   end
 
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -5,8 +5,12 @@ require_relative("../models/game.rb")
 
 class GameTest < MiniTest::Test
 
-  def test_game()
+  def test_game__p1_rock_wins_over_scissors()
     assert_equal("Rock wins!", Game.play("rock", "scissors"))
+  end
+
+  def test_game__p1_rock_loses_over_paper()
+    assert_equal("Paper wins!", Game.play("rock", "paper"))
   end
 
 end
