84d2f2cec3d248b7f31ab096e86ea3f49889a5de
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-16 20:10:22 +0100

    styling complete

--- models/game.rb
+++ models/game.rb
@@ -1,14 +1,15 @@
 class Game
 
   def self.play(input1, input2)
-    return "It's a Draw!" if input1 == input2
 
-    return "Rock over Scissors, Player 1 wins!" if input1 == "rock" && input2 == "scissors"
-    return "Paper over Rock, Player 2 wins!" if input1 == "rock" && input2 == "paper"
-    return "Scissors over Paper, Player 1 wins!" if input1 == "scissors" && input2 == "paper"
-    return "Rock over Scissors, Player 2 wins!" if input1 == "scissors" && input2 == "rock"
-    return "Paper over Rock, Player 1 wins!" if input1 == "paper" && input2 == "rock"
-    return "Scissors over Paper, player 2 wins!" if input1 == "paper" && input2 == "scissors"
+    return "It's a <span>Draw!</span>" if input1 == input2
+
+    return "Rock <span>beats</span> Scissors, <br><span>Player 1 wins!</span>" if input1 == "rock" && input2 == "scissors"
+    return "Paper <span>beats</span> Rock, <br><span>Player 2 wins!</span>" if input1 == "rock" && input2 == "paper"
+    return "Scissors <span>beats</span> Paper, <br><span>Player 1 wins!</span>" if input1 == "scissors" && input2 == "paper"
+    return "Rock <span>beats</span> Scissors, <br><span>Player 2 wins!</span>" if input1 == "scissors" && input2 == "rock"
+    return "Paper <span>beats</span> Rock, <br><span>Player 1 wins!</span>" if input1 == "paper" && input2 == "rock"
+    return "Scissors <span>beats</span> Paper, <br><span>Player 2 wins!</span>" if input1 == "paper" && input2 == "scissors"
 
   end
 
