c65a19a27db64e2125909652cbd5de631ef26551
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-16 16:52:50 +0100

    create app.rb and result.erb. can play in browser

---
+++ app.rb
@@ -0,0 +1,10 @@
+require("sinatra")
+require("sinatra/contrib/all") if development?
+also_reload("./models/*")
+
+require_relative("./models/game.rb")
+
+get "/game/:input1/:input2" do
+  @game_result = Game.play(params[:input1], params[:input2])
+  erb(:result)
+end
--- models/game.rb
+++ models/game.rb
@@ -3,7 +3,6 @@ class Game
   def self.play(input1, input2)
     return "It's a Draw!" if input1 == input2
 
-
     return "Rock over Scissors, Player 1 wins!" if input1 == "rock" && input2 == "scissors"
     return "Paper over Rock, Player 2 wins!" if input1 == "rock" && input2 == "paper"
     return "Scissors over Paper, Player 1 wins!" if input1 == "scissors" && input2 == "paper"
